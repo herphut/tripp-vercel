4aae3122cce8df27e2fb051fa1ff7abe046f4c29
herphut <admin@herphut.com>
2025-09-08 14:57:57 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "2rem",
-                  fontfamily: "sans-serif" }}>
+                  fontFamily: "sans-serif" }}>
       <h1> Tripp is alive!</h1>
       <p>Next.js app is running on Vercel</p>
     </main>
