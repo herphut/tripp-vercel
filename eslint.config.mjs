b3448bb3bd1292a18305f363b187e61e5f2d933c
Herphut <admin@herphut.com>
2025-09-14 14:32:37 -0700

    chore: remove no-explicit-any rule from ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,6 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
      rules: {
-      "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities": "off",
      },
   },
